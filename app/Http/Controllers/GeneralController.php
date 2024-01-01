31be2d74545fa9d55a198c50f7c73e2c3234d8e7
AbdullahAldakheel <a.dukhiel9@gmail.com>
2024-01-01 17:16:16 +0300

    Stock API

--- app/Http/Controllers/GeneralController.php
+++ app/Http/Controllers/GeneralController.php
@@ -2,12 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Helper\CalculationsHelper;
+use App\Models\Ingredient;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Response;
 
 class GeneralController extends Controller
 {
+    /**
+     * @description Current Stock
+     * @return JsonResponse
+     */
+    public function stock(): JsonResponse
+    {
+        $ingredients = Ingredient::with('productIngredients.product.orders')->get();
+        $data = [];
+        foreach ($ingredients as $ingredient) {
+            $item = [
+                'ingredient' => $ingredient->item,
+                'weight' => CalculationsHelper::gramToKgConverter(weight_in_grams: $ingredient->weight_in_grams),
+                'ordered_weight' => CalculationsHelper::gramToKgConverter(weight_in_grams: $ingredient->ordered_weight_sum),
+            ];
+            $threshold = $ingredient->weight_in_grams * 0.5; // 50% of the weight in the Ingredient table
+            $item['weight_reached_threshold'] = $ingredient->ordered_weight_sum >= $threshold;
+            $item['percentage_used'] = $ingredient->ordered_weight_sum / $ingredient->weight_in_grams * 100 . '%';
+            $data[] = $item;
+        }
+
+        return Response::json($data);
+    }
+
 
     /**
      * @description Health check endpoint
